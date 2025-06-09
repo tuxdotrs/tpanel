1d3cb278e46cc86492e4bfc7e674ac1b884441ac
tux <t@tux.rs>
2025-06-09 20:20:52 +0530

    feat: add os icon

--- widgets/bar/launcher.tsx
+++ widgets/bar/launcher.tsx
@@ -1,21 +1,13 @@
-import { App, Gtk } from "astal/gtk4";
-import { Gio } from "astal";
-import { Picture } from "../common";
+import { App } from "astal/gtk4";
 import { WINDOW_NAME } from "../app-launcher";
 
 export const Launcher = () => {
   return (
-    <Gtk.ScrolledWindow heightRequest={40} widthRequest={40}>
-      <button
-        cssClasses={["launcher"]}
-        onClicked={() => App.toggle_window(WINDOW_NAME)}
-      >
-        <Picture
-          file={Gio.file_new_for_path("/home/tux/Wallpapers/avatar.png")}
-          contentFit={Gtk.ContentFit.CONTAIN}
-          overflow={Gtk.Overflow.HIDDEN}
-        />
-      </button>
-    </Gtk.ScrolledWindow>
+    <button
+      cssClasses={["pill", "launcher"]}
+      onClicked={() => App.toggle_window(WINDOW_NAME)}
+    >
+      <image iconName="nix-symbolic" />
+    </button>
   );
 };
