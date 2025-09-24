e09d781605ca315e8f3d87d9a4c8c7658ce2b312
tux <t@tux.rs>
2025-09-24 21:57:45 +0530

    refactor: move styles to seperate folder

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import app from "ags/gtk4/app";
 import GLib from "gi://GLib";
-import style from "./style.scss";
+import style from "./style/main.scss";
 import windows from "./windows";
 
 const icons = `${GLib.get_user_config_dir()}/tpanel/assets/icons`;
