864f39fe8d994a793e94c85b640ae8e1e590949f
tux <t@tux.rs>
2025-09-27 21:03:15 +0530

    feat(network): add clickable network widget and launch nmtui

--- widgets/bar/network.tsx
+++ widgets/bar/network.tsx
@@ -1,3 +1,5 @@
+import { Gdk } from "ags/gtk4";
+import { execAsync } from "ags/process";
 import AstalNetwork from "gi://AstalNetwork";
 import { createBinding } from "gnim";
 
@@ -19,7 +21,9 @@ export const Network = () => {
     const wifi = net.wifi;
     switch (wifi.internet) {
       case 0:
-        return wifi.ssid;
+        const ssid = wifi.ssid;
+        let text = ssid.length > 10 ? ssid.substring(0, 10) + "..." : ssid;
+        return text;
       case 1:
         return "Connecting";
       case 2:
@@ -30,11 +34,17 @@ export const Network = () => {
   };
 
   return (
-    <box cssClasses={["pill"]}>
+    <button
+      cssClasses={["pill"]}
+      cursor={Gdk.Cursor.new_from_name("pointer", null)}
+      onClicked={() =>
+        execAsync(`wezterm start --class wezterm-floating nmtui`)
+      }
+    >
       <box spacing={5}>
         <image iconName="fa-wifi-symbolic" />
         <label label={connnectivity((c) => getNetworkText(c, network))} />
       </box>
-    </box>
+    </button>
   );
 };
